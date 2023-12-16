a38928103ba1c7f00db1d5f878fe744a166c2507
Maciej_Michon <maciek_michon@o2.pl>
2023-12-16 17:49:57 +0100

    test 2

--- DarwinWorld/src/main/java/oop/model/MapDirection.java
+++ DarwinWorld/src/main/java/oop/model/MapDirection.java
@@ -1,4 +1,19 @@
 package oop.model;
 
 public enum MapDirection {
+    NORTH("N"),
+    NORTH_EAST("NE"),
+    EAST("E"),
+    SOUTH_EAST("SE"),
+    SOUTH("S"),
+    SOUTH_WEST("SW"),
+    WEST("W"),
+    NORTH_WEST("NW");
+
+
+    public final String direction;
+    MapDirection(String direction){
+        this.direction = direction;
+    }
+
 }
