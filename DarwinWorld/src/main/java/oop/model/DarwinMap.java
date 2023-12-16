eefac77909bdf5e36be67ba2b6e003c8e0e7c981
aleks <aleksandra.fafara11@gmail.com>
2023-12-16 17:46:52 +0100

    commit2

--- DarwinWorld/src/main/java/oop/model/DarwinMap.java
+++ DarwinWorld/src/main/java/oop/model/DarwinMap.java
@@ -1,4 +1,14 @@
 package oop.model;
 
 public class DarwinMap {
+    private Vector2d bottomLeft;
+    private Vector2d upperRight;
+    public int mapId;
+    public DarwinMap(int width, int height, int mapId){
+        this.mapId = mapId;
+        this.upperRight = new Vector2d();
+        this.bottomLeft = new Vector2d();
+        //generateGrass
+        //generateWater if()
+    }
 }
